5a6d94073576229c91dd696e51b49a192245e177
Fatima Garcia <fagarmartin@gmail.com>
2023-07-06 10:37:15 +0200

    data changed to json

--- src/components/SectionCard.jsx
+++ src/components/SectionCard.jsx
@@ -1,13 +1,13 @@
-function SectionCard({ info: { title, company, date, city,achievements }}) {
+function SectionCard({ id,title, company, date, city,achievements}) {
   return (
     <section>
       <h4>{title}</h4>
       <div className="info-details">
         <h5>{date}</h5> <h5>{company}</h5> <h5>{city}</h5>
       </div>
-      <ul className="achievements-tasks">{achievements.map((each)=>{
+      <ul className="achievements-tasks">{achievements.map((each,i)=>{
         return (
-          <li>{each}</li>
+          <li  key={`achv${i}`}>{each}</li>
         )
       })}</ul>
     </section>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,23 @@
 import Header from "../components/Header";
 import SectionCard from "../components/SectionCard";
+import data from "../data/workexperience.json";
 function Home() {
-  const achievementsEstevez=["Succesfully designed and developed projects for the departments of production, laboratory and  maintenance.","Database design with SQL Management.","Logic implementation.","Testing and debugging.","Implementation and deployment."]
+
   return (
     <div className="container">
       <Header />
       <div className="my-experience">
-      {/* jose estevez */}
-        <SectionCard info={{title:"Full-Stack Developer",company:"José Estévez S.A.",date:"05/2016 - 12/2022",city:"Jerez de la Frontera,Cádiz",achievements:achievementsEstevez}}/>
+        {/* jose estevez */}
+        {data.map(({ id, title, company, date, city, achievements }) => (
+          <SectionCard
+            key={id}
+            title={title}
+            company={company}
+            date={date}
+            city={city}
+            achievements={achievements}
+          />
+        ))}
       </div>
     </div>
   );

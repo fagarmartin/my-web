c279c1a6b0f0a64d777564889eb68994bdbd79e0
Fatima Garcia <fagarmartin@gmail.com>
2023-07-25 18:15:58 +0200

    test removing mobile cursor

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -1,25 +1,36 @@
 import { useEffect, useState } from "react";
-
+import { isMobile } from "react-device-detect";
 function Cursor() {
   const [mousePosition, setMousePosition] = useState({
     x: 0,
     y: 0,
   });
-const [cursorVariant,setCursorVariant]=useState("default")
+  const [showCursor, setShowCursor] = useState(false);
+  //const [cursorVariant,setCursorVariant]=useState("default")
 
   useEffect(() => {
-    const mouseMoveHandle = e => {
-      setMousePosition({x:e.clientX,y:e.clientY})
+    const mouseMoveHandle = (e) => {
+      setMousePosition({ x: e.clientX, y: e.clientY });
     };
     window.addEventListener("mousemove", mouseMoveHandle);
+    /* if is on mobile*/
+
+    isMobile ? setShowCursor(false) : setShowCursor(true);
+
     return () => {
-      window.removeEventListener("mousemove",null);
+      window.removeEventListener("mousemove", null);
     };
   }, []);
 
-
-  
-  return <div className="cursor" style={{top:mousePosition.y-16, left:mousePosition.x-16}} />;
+  if (showCursor) {
+    return (
+      <div
+        className="cursor"
+        style={{ top: mousePosition.y - 16, left: mousePosition.x - 16 }}
+      />
+    );
+  }
+  return <></>;
 }
 
 export default Cursor;
--- src/components/LanguageCard.jsx
+++ src/components/LanguageCard.jsx
@@ -1,13 +1,10 @@
-
-function LanguageCard({language,title}) {
+function LanguageCard({ language, title }) {
   return (
-    <div className="list-cards" >
-    <h4>{language}</h4>
-    <h5>{title}</h5>
-
+    <div className="list-cards">
+      <h4>{language}</h4>
+      <h5>{title}</h5>
     </div>
-   
-  )
+  );
 }
 
-export default LanguageCard
+export default LanguageCard;
